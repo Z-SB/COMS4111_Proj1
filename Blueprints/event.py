cd2da163424c1b21c63232789ee9370b62262dc3
sqwy <514728172@qq.com>
2022-04-13 02:16:41 -0400

    创建了createEvent方法和对应的html文件 稍稍更改了index 以及加入了selectCourt.html文件，明天准备写对应的方法

--- Blueprints/event.py
+++ Blueprints/event.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, g, session
+from flask import Blueprint, render_template, g, session,request
 
 bp = Blueprint('event', __name__, url_prefix='/')
 
@@ -10,7 +10,19 @@ def index():
         from users U join create_events E on U.owner = E.owner
         where U.email_address = %s
         """,session.get('user_email')).fetchall()
-        print(user_events)
+        return render_template('index.html',event = user_events)
     return render_template('index.html')
 
 
+@bp.route('/createNewEvent', methods=['GET', 'POST'])
+def create_new_event():
+    if request.method == 'POST':
+        time = request.form['time']
+        title = request.form['title']
+        sport = request.form['sport']
+        g.conn.execute("""
+        insert into create_events values (%s,%s,%s)
+        """, (1, title, sport))
+        return render_template('selectCourt.html')
+
+    return render_template('createEvent.html')
